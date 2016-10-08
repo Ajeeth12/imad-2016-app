cac1f121ad0644054a819a61ce160d2371281a02
Ajeeth12 <ajeethaj99@gmail.com>
2016-10-08 04:34:30 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the element of the main-text
 var element = document.getElementById('main-text');
 element.innerHTML = 'I am an Indian';
+// move the image
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginleft='100px';
+};
