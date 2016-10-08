aa2e0d761e217dcbaf872d425457f45e3791d58d
Ajeeth12 <ajeethaj99@gmail.com>
2016-10-08 04:52:22 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ element.innerHTML = 'I am an Indian';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 };
